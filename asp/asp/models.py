0eda893dcfbd85e36f3ff523308d63425f63bbd6
Ru Chiang <jgchengru@gmail.com>
2018-10-28 13:29:05 +0800

    Added Models:
    Added User, Item, Ordered_Item, and Order models

--- asp/asp/models.py
+++ asp/asp/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    CLINIC_MANAGER = 'CM'
+    DISPATCHER = 'DP'
+    WAREHOUSE_PERSONNEL = 'WP'
+    ADMIN = 'AD'
+    ROLES = (
+        (CLINIC_MANAGER, 'Clinic Manager'),
+        (DISPATCHER, 'Dispatcher'),
+        (WAREHOUSE_PERSONNEL, 'Warehouse Personnel'),
+        (ADMIN, 'Admin')
+    )
+
+    username = models.CharField(max_length = 50)
+    email = models.EmailField(max_length = 254)
+
+    role = models.CharField(
+        max_length = 2,
+        choices = ROLES,
+        default = CLINIC_MANAGER
+    )
+
+
+    def __str__(self):
+        return f"{self.name}: {self.role}"
+
+class Item(models.Model):
+    name = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 200)
+    weight = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Ordered_Item(models.Model):
+    item = models.OneToOneField(Item)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.quantity}"
+
+class Order(models.Model):
+    QUEUED_FOR_PROCESSING = 'QFP'
+    PROCESSING_BY_WAREHOUSE = 'PBW'
+    QUEUED_FOR_DISPATCH = 'QFD'
+    DISPATCHED = 'DSD'
+    DELIVERED = 'DLD'
+
+    STATUS = (
+        (QUEUED_FOR_PROCESSING, 'Queued for Processing'),
+        (PROCESSING_BY_WAREHOUSE, 'Processing by Warehouse'),
+        (QUEUED_FOR_DISPATCH, 'Queued for Dispatched'),
+        (DISPATCHED, 'Dispatched'),
+        (DELIVERED, 'Delivered')
+    )
+
+    status = models.CharField(
+        max_length = 3,
+        choices = STATUS,
+        default = QUEUED_FOR_PROCESSING
+    )
+
+    requester = models.ForeignKey(User, on_delete = models.CASCADE)
+    time = models.DateTimeField()
+    ref_no = models.PositiveIntegerField(unique = True)
+    items = models.ManyToManyField(Ordered_Item)
+
+    def __str__(self):
+        return f"{self.ref_no}, {self.status}"
--- asp/config/settings.py
+++ asp/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'asp.apps.AspConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
