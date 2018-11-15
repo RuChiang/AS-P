51934ee355ad7cdcec156229aecb0f1cda4fe8a2
Ajit Krishna <ajitkrishna99@gmail.com>
2018-11-15 15:11:12 +0800

    Testing frontend disable button

--- asp/asp/static/asp/script.js
+++ asp/asp/static/asp/script.js
@@ -1,11 +1,8 @@
-console.log('im here')
-
-$('body').on('keyup', '.orderQuantity', function() {
-    $(this).attr('id');
-    $('#placeOrder').prop('disabled', true);
-    var sumWeight;
-    $('.orderQuantity').each(function() {
-        sumWeight += $(this).val() * $('.' + $(this).attr('id'));
-    });
-    console.log(sumWeight);
-});
+// $('body').on('keyup', '.orderQuantity', function() {
+//     // $('#placeOrder').prop('disabled', true);
+//     var sumWeight;
+//     $('.orderQuantity').each(function() {
+//         sumWeight += $(this).val() ;
+//     });
+//     console.log(sumWeight);
+// });
